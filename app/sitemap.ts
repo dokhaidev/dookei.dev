cd386752cdbaab809060bb5386c19b8ebad724b8
dokhaidev <khaihuynh08062005@gmail.com>
2026-04-09 13:06:18 +0700

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,14 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = "2026-04-09";
+
   return [
     {
       url: "https://example.com/",
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: "monthly",
       priority: 1,
     },
